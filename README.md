ff75aed9763ccafcc88244b11dffd9b1ac89b701
alexr1993 <alexr1993@gmail.com>
2013-09-25 22:39:33 +0100

    Trying to find comments which have been tagged as references by other people, need to drill down into the load more comments rather than look at the surface ones though. Need to find out what order they are listed in

--- README.md
+++ README.md
@@ -2,3 +2,13 @@ Reddit-Bot
 ==========
 
 Finds references made about popular threads
+
+We have references people make in comments to posts either popular recently or
+possibly a post which is famous/infamous in Reddit history. The first part of
+this problem is finding the most simple references, i.e. those which can be 
+linked using the title text, after that it will get weird with references to
+images and references made using images.
+
+To start off I am going to cheat by using references people have pointed out
+already, perhaps by text or by posting that captain america "I get that refe-
+rence" gif
--- bot.py
+++ bot.py
@@ -1,12 +1,70 @@
 # Bot which catches people making references to front page articles in
 # comments of other (front page) comments
 
-user_agent = "reference-getter bot by OfficeGuyChillin"
+import praw
+import re     # regex
+
+
+
+
+
+user_agent = "let alexr1993@gmail.com know if I'm breaking the rules"
 
 user  = "Ref_Bot"
+
 
 # Keeping password safe on my PC as code is free on GitHub
 pword_file = open("C:\\redditbotpassword.txt", 'r')
 pword = pword_file.read()
 
-print(pword)
+
+
+
+reddit = praw.Reddit(user_agent)
+reddit.login(user, pword)
+print("Logged in as " + user)
+
+
+## part 1: Get all front page posts and start sifting through the comments
+
+submissions = reddit.get_front_page()
+
+i = 0
+num_of_comments = []
+
+for submission in submissions:
+    j = 0
+    
+    print(submission.title)
+    print(submission.subreddit)
+    print (str(i) + "th Submission")
+
+
+    i = i + 1
+    sub_comments = submission.comments
+
+    for comment in sub_comments:
+        
+        if not isinstance(comment, praw.objects.MoreComments): #"reference" in comment.body:
+            cond = re.search("\sreference", comment.body)
+            if (cond):
+                print (comment.body)
+            
+        else: # must be end of first page of comments
+            num_of_comments.append(j)
+            print(str(j) + " Comments")
+
+        j = j + 1
+
+
+    print("\n")
+    print("#" * 80)
+    print("\n")
+
+
+rolling_total = 0
+
+for com in num_of_comments:
+    rolling_total += com
+
+print("Total comments: " + str(rolling_total))
