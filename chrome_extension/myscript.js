f194e81612ab516cd42b54d67bd425b8dc0e5175
larry <alexr1993@gmail.com>
2016-08-09 21:05:18 +0100

    Implement extension

--- chrome_extension/myscript.js
+++ chrome_extension/myscript.js
@@ -1,8 +1,18 @@
 let titles = document.querySelectorAll("a.title");
 titles.forEach(function (t) { console.log( t.href) });
 titles.forEach(function (t) {
-  var newElement = document.createElement("b");
-  newElement.textContent = "(blah) ";
-  var parent = t.parentElement;
-  parent.insertBefore(newElement, t);
+  $.get( "http://127.0.0.1:5000/", { url: t.href } )
+  .done(function( data ) {
+    let subreddits = JSON.parse(data)
+    subreddits.forEach(function(subreddit) {
+      var newElement = document.createElement("a");
+      newElement.style.color = "red";
+      newElement.classList = ["subreddit", "hover"];
+      newElement.textContent = "(" + subreddit + ") ";
+      newElement.href = "https://www.reddit.com" + subreddit;
+      var parent = t.parentElement;
+      parent.insertBefore(newElement, t);
+    });
+  });
+
 });
--- src/experiment.py
+++ src/experiment.py
@@ -1,38 +1,79 @@
+from flask import Flask
+from flask import request
+app = Flask(__name__)
+
+from flask.ext.cors import CORS
+CORS(app)
+
+import json
 import praw
 
 r = praw.Reddit(user_agent='my_cool_application')
 
-# submissions = r.get_subreddit('opensource').get_hot(limit=5)
-# [str(x) for x in submissions]
+cache = {}
 
-sub = "soccer"
-subreddit = r.get_subreddit(sub)
-posts = subreddit.get_top_from_day(limit=10)
+@app.route("/")
+def hello():
+    link = request.args.get("url")
+    if link in cache:
+        return cache[link]
 
-output = [sub]
-
-def log(line):
-    output.append(line)
-    print(line)
-
-for p in posts:
-    link = p.url
-    log(link)
-    log(p.title)
     matches = r.search(link)
 
     iterator = iter(matches)
+    output = []
     while True:
         try:
             match = next(iterator)
             url = match.subreddit.url
-            log("    " + url)
+            output.append(url)
+            print("    " + url)
         except StopIteration:
             break
         except praw.errors.RedirectException:
             url = thread.getSubredditUrl()
-            log("    " + url)
+            output.append(url)
+            print("    " + url)
+            
+    response =  json.dumps(output)
+    cache[link] = response
+    return response
 
-#print('\n'.join(output))
-# matches = r.search("https://streamable.com/e7e0")
-# [print(m.subreddit.url) for m in matches]
+if __name__ == "__main__":
+    app.run()
+
+
+# # submissions = r.get_subreddit('opensource').get_hot(limit=5)
+# # [str(x) for x in submissions]
+
+# sub = "soccer"
+# subreddit = r.get_subreddit(sub)
+# posts = subreddit.get_top_from_day(limit=10)
+
+# output = [sub]
+
+# def log(line):
+#     output.append(line)
+#     print(line)
+
+# for p in posts:
+#     link = p.url
+#     log(link)
+#     log(p.title)
+#     matches = r.search(link)
+
+    # iterator = iter(matches)
+    # while True:
+    #     try:
+    #         match = next(iterator)
+    #         url = match.subreddit.url
+    #         log("    " + url)
+    #     except StopIteration:
+    #         break
+    #     except praw.errors.RedirectException:
+    #         url = thread.getSubredditUrl()
+    #         log("    " + url)
+
+# #print('\n'.join(output))
+# # matches = r.search("https://streamable.com/e7e0")
+# # [print(m.subreddit.url) for m in matches]
