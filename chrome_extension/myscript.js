8949bc9900e8ba1be1518672060f246ea8c88f1b
larry <alexr1993@gmail.com>
2016-08-13 18:18:47 +0100

    Make it work on neverending reddit, fix metadata

--- chrome_extension/myscript.js
+++ chrome_extension/myscript.js
@@ -1,24 +1,47 @@
-let service = "http://127.0.0.1:8000/reddit";
-let titles = document.querySelectorAll("a.title.srTagged"); // Visible posts seem to have srTagged class
-let internalPost = /https?:\/\/(www|np)\.reddit\.com\/r\//;
+let service = "https://jukeboxxy.com/reddit";
+let internalPost = /https?:\/\/((www|np)\.)?reddit\.com\/r\//;
 
-titles.forEach(function (t) {
-  if (internalPost.exec(t.href) !== null) {
-    return; // Reddit doesn't support searching for subreddits with self posts (very well)
+var fetchedPosts = [];
+
+var fetchSubreddits = function() {
+  var posts = document.querySelectorAll("a.title.srTagged"); // Visible posts seem to have srTagged class
+  if (posts.length <= fetchedPosts.length) {
+    return;
   }
-  console.log("Searching "+ t.href);
 
-  $.get(service, { url: t.href } )
-  .done(function( data ) {
-    let subreddits = JSON.parse(data)
-    subreddits.forEach(function(subreddit) {
-      var newElement = document.createElement("a");
-      newElement.style.color = "red";
-      newElement.classList = ["subreddit", "hover"];
-      newElement.textContent = "(" + subreddit + ") ";
-      newElement.href = "https://www.reddit.com" + subreddit;
-      var parent = t.parentElement;
-      parent.appendChild(newElement);
+  let startIx = fetchedPosts.length;
+  fetchedPosts = posts;
+
+  var newPosts = [];
+  if (startIx === 0) {
+    newPosts = posts;
+  } else {
+    newPosts = Array.from(posts).slice(startIx - 1);
+  }
+
+  console.log("newPosts" + newPosts.length);
+
+  newPosts.forEach(function (t) {
+    if (internalPost.exec(t.href) !== null) {
+      return; // Reddit doesn't support searching for subreddits with self posts (very well)
+    }
+    console.log("Searching "+ t.href);
+
+    // TODO make this work with never ending reddit
+    $.get(service, { url: t.href } )
+    .done(function( data ) {
+      let subreddits = JSON.parse(data)
+      subreddits.forEach(function(subreddit) {
+        var newElement = document.createElement("a");
+        newElement.style.color = "red";
+        newElement.classList = ["subreddit", "hover"];
+        newElement.textContent = " " + subreddit;
+        newElement.href = "https://www.reddit.com" + subreddit;
+        var parent = t.parentElement;
+        parent.appendChild(newElement);
+      });
     });
   });
-});
+};
+
+setInterval(fetchSubreddits, 1000);
