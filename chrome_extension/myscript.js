28a7083364d182d04c83417625183141d51f9cd4
larry <alexr1993@gmail.com>
2016-08-16 22:00:24 +0100

    Improve UX

--- chrome_extension/myscript.js
+++ chrome_extension/myscript.js
@@ -6,12 +6,50 @@ var fetchedPosts = {};
 // TODO images hosted on i.reddit will not be linked from the <a>.
 // The post links to the comments, but the thumbnail/expando loads the image both on reddit and RES
 
+var createCell = function() {
+  let cell = document.createElement("a");
+  cell.style.color = "rgb(0, 204, 102)";
+  return cell;
+};
+
+var formatDate = function(date) {
+  let now = new Date(Date.now());
+
+  let diffSecs = (now.getTime() - date.getTime()) / 1000;
+  if (diffSecs < 60) {
+    return Math.floor(diffSecs) + " seconds ago";
+  }
+
+  let diffMins = diffSecs/60;
+  if (diffMins < 60) {
+    return Math.floor(diffMins) + " minutes ago";
+  }
+
+  let diffHours = diffMins/60;
+  if (diffHours < 24) {
+    return Math.floor(diffHours) + " hours ago";
+  }
+
+  let diffDays = diffHours/24;
+  if (diffDays < 32) {
+    return Math.floor(diffDays) + " days ago";
+  }
+
+  let diffMonths = diffDays/30;
+  if (diffMonths < 13) {
+    return Math.floor(diffMonths) + " months ago";
+  }
+
+  return date.toUTCString();
+  //let diffYears = diffMonths/12;
+  //return Math.floor(diffYears) + " years ago";
+};
+
 var presentSubmissionData = function(linkTag, subData) {
   let parentElement  = linkTag.parentElement.parentElement;
   if (!Array.from(parentElement.classList).includes("entry")) {
     console.log("Cannot find submission top-level container for post " + linkTag.href);
   }
-  console.log(parentElement);
 
   subData.sort(function(a, b) {
     let keyA = a.created_utc, keyB = b.created_utc;
@@ -21,21 +59,35 @@ var presentSubmissionData = function(linkTag, subData) {
     return 1;
   });
 
-  let list = document.createElement("ul");
-  parentElement.appendChild(list);
+  let table = document.createElement("table");
+  parentElement.appendChild(table);
   subData.forEach(function(subDatum) {
     let subreddit = subDatum.subredditName;
-    var newElement = document.createElement("a");
+    let dateCell = createCell();
+    let subredditCell = createCell();
+    let scoreCell = createCell();
 
     let date = new Date(subDatum.created_utc * 1000);
+    dateCell.textContent = formatDate(date);
+    dateCell.href = subDatum.permalink;
 
-    newElement.style.color = "red";
-    newElement.classList = ["subreddit", "hover"];
-    newElement.textContent = date.toUTCString() + " " + subreddit + " " + subDatum.score;
-    newElement.href = "https://www.reddit.com/r/" + subreddit;
-    let item = document.createElement("li");
-    item.appendChild(newElement);
-    list.appendChild(item);
+    subredditCell.textContent = "/r/" + subreddit;
+    subredditCell.href = "https://www.reddit.com/r/" + subreddit;
+
+    scoreCell.textContent = subDatum.score;
+
+    let td = function(element) {
+      let cell = document.createElement("td");
+      cell.appendChild(element);
+      cell.style.paddingRight = "10px";
+      return cell;
+    }
+
+    let row = document.createElement("tr");
+    row.appendChild(td(dateCell));
+    row.appendChild(td(subredditCell));
+    row.appendChild(td(scoreCell));
+    table.appendChild(row);
   });
 }
 
