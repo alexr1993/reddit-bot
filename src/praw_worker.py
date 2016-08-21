382407124c5f43d70eb3fbf829b8d1ebd2117ff2
larry <alexr1993@gmail.com>
2016-08-21 14:45:47 +0100

    Adapt to new caching strategy

--- src/praw_worker.py
+++ src/praw_worker.py
@@ -6,35 +6,29 @@ import json
 import datetime
 import time
 
-r = praw.Reddit(user_agent='subreddit finder chrome extension')
-cache = redis.StrictRedis(host='localhost', port=6379, db=0)
-queue_name = "request queue"
-
-connection = pika.BlockingConnection(pika.ConnectionParameters(
-        host='localhost'))
-channel = connection.channel()
-
-channel.queue_declare(queue_name)
-
 def callback(ch, method, properties, body):
     print(" [x] Received %r" % body)
-
     cached = cache.get(body)
     # TODO check timestamp is older than x (also do this frontend)
-    if cached is not None:
+    if cached is not None and "submission_data_list" in cached.decode("utf-8"):
+        print("Ignoring cached url " + str(body))
         return
 
     matches = r.search(body)
 
     iterator = iter(matches)
     output = []
+    cache_data = {
+        "cache_timestamp_utc": int(time.mktime(datetime.datetime.utcnow().timetuple())),
+        "submission_data_list": output
+    }
+
     while True:
         try:
             match = next(iterator)
 
             submissionData = {
-                "subredditName": str(match.subreddit),
-                "cache_timestamp_utc": int(time.mktime(datetime.datetime.utcnow().timetuple())),
+                "subreddit_name": str(match.subreddit),
                 "permalink": match.permalink,
                 "score": match.score,
                 "url": match.url,
@@ -48,19 +42,21 @@ def callback(ch, method, properties, body):
         except praw.errors.RedirectException:
             continue
             
-    # todo insert link => output into the cache
-    searchData = json.dumps(output)
+    searchData = json.dumps(cache_data)
     print("caching: " + searchData)
-    cache.set(body, searchData)
+    cache.set(body, searchData, ex=3600)
 
-channel.basic_consume(callback,
-                      queue=queue_name,
-                      no_ack=True)
 
 print(' [*] Waiting for messages. To exit press CTRL+C')
 while True:
     try:
+        r = praw.Reddit(user_agent='subreddit finder chrome extension')
+        cache = redis.StrictRedis(host='localhost', port=6379, db=0)
+        queue_name = "request queue"
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+        channel = connection.channel()
+        channel.queue_declare(queue_name)
+        channel.basic_consume(callback, queue=queue_name, no_ack=True)
         channel.start_consuming()
     except Exception as ex:
         print(ex)
-        print("wtf")
