7fc36f69d87e3c0249e78425188bc8410435b49a
larry <alexr1993@gmail.com>
2016-08-31 22:08:08 +0100

    Clean up DI module

--- subreddit-finder-services/src/main/java/com/alexremedios/subredditfinderservice/AppConfig.java
+++ subreddit-finder-services/src/main/java/com/alexremedios/subredditfinderservice/AppConfig.java
@@ -1,12 +1,8 @@
 package com.alexremedios.subredditfinderservice;
 
 import com.rabbitmq.client.ConnectionFactory;
-import org.springframework.boot.actuate.autoconfigure.ExportMetricWriter;
-import org.springframework.boot.actuate.metrics.repository.redis.RedisMetricRepository;
-import org.springframework.boot.actuate.metrics.writer.MetricWriter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 
